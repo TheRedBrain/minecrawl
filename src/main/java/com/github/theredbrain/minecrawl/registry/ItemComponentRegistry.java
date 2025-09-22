63abccc7361a3691ad83ece31153797bbdbff051
TheRedBrain <thetrueredbrain@gmail.com>
2025-09-22 19:01:28 +0200

    fixed data component

--- src/main/java/com/github/theredbrain/minecrawl/registry/ItemComponentRegistry.java
+++ src/main/java/com/github/theredbrain/minecrawl/registry/ItemComponentRegistry.java
@@ -2,7 +2,6 @@ package com.github.theredbrain.minecrawl.registry;
 
 import com.github.theredbrain.minecrawl.Minecrawl;
 import com.github.theredbrain.minecrawl.component.type.InfiniteSpellAmmoContainerComponent;
-import com.github.theredbrain.scriptblocks.ScriptBlocks;
 import net.minecraft.component.ComponentType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -11,7 +10,7 @@ public class ItemComponentRegistry {
 	public static void init() {
 		Minecrawl.INFINITE_SPELL_AMMO_CONTAINER_COMPONENT = Registry.register(
 				Registries.DATA_COMPONENT_TYPE,
-				ScriptBlocks.identifier("infinite_spell_ammo_container_component"),
+				Minecrawl.identifier("infinite_spell_ammo_container_component"),
 				ComponentType.<InfiniteSpellAmmoContainerComponent>builder().codec(InfiniteSpellAmmoContainerComponent.CODEC).packetCodec(InfiniteSpellAmmoContainerComponent.PACKET_CODEC).cache().build()
 		);
 	}
